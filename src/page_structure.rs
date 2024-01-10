1294afff7777e43ad70aecc3b048912c37ff4eb7
asempere123123 <asempere158@gmail.com>
2024-01-10 13:41:51 +0100

    Estructura

--- src/page_structure.rs
+++ src/page_structure.rs
@@ -1,4 +1,5 @@
 use yew::prelude::*;
+use yew_bootstrap::component::{Container, NavBar, NavItem, ContainerSize};
 
 #[derive(Properties, PartialEq)]
 pub struct PabeStructureProps {
@@ -10,6 +11,13 @@ pub struct PabeStructureProps {
 pub fn page_structure(props: &PabeStructureProps) -> Html {
 
     html! {
-        props.children.clone()
+    <>
+        <NavBar nav_id={"test-nav"} class="navbar-expand-lg navbar-light bg-light">
+            <NavItem text="link 1" />
+        </NavBar>
+        <Container>
+            {props.children.clone()}
+        </Container>
+    </>
     }
 }
